8db066e80318f2c1e7c3172d43587e7f0146b80f
Diego <diego@CSS-IT-Roberto.attlocal.net>
2020-12-09 17:43:58 -0800

    all of Diego's code

---
+++ src/init.c
@@ -0,0 +1,13 @@
+
+#include "main.h"
+
+void initializeIO() {
+  pinMode(LIMIT_SWITCH, INPUT);
+  pinMode(6, INPUT);
+}
+
+void initialize() {
+  encoder1 = encoderInit(QUAD_TOP_PORT, QUAD_BOTTOM_PORT, false);
+  encoder2 = encoderInit(QUAD_TOP_PORT2, QUAD_BOTTOM_PORT2, false);
+
+}
---
+++ src/opcontrol.c
@@ -0,0 +1,118 @@
+#include "main.h"
+int con = 2;
+int target;
+int target2;
+
+
+void homeShoulder(int homePosition) {
+printf("homing started, stand by \n");
+while(digitalRead(3)==1){
+  printf("The bumper switch value %d \n", digitalRead(3)==1);
+motorSet(9,-63);
+}
+
+motorSet(9,0);
+encoderReset(encoder1);
+while(encoderGet(encoder1)<homePosition) {
+  printf("The shoulder encoder value %d \n", encoderGet(encoder1));
+motorSet(9,63);
+}
+motorSet(9,0);
+printf("homing complete \n");
+}
+
+void homeElbow(int homePosition){
+  printf("homing started, stand by \n");
+  while(digitalRead(6)==HIGH){
+  printf("The limit switch value %d \n", digitalRead(6));
+  motorSet(8,-63);
+  }
+  motorSet(8,0);
+  encoderReset(encoder2);
+  while(encoderGet(encoder2)>homePosition) {
+    printf("The elbow encoder value %d \n", encoderGet(encoder2));
+  motorSet(8,63);
+  }
+  motorSet(8,0);
+  printf("homing complete \n");
+  }
+
+
+void chassisSet(int left, int right) {
+  motorSet(2, left);
+  motorSet(3, right);
+}
+
+
+void shoulderSet(int speed) {
+	motorSet(9, -speed);
+}
+
+void wristSet(int speed) {
+	motorSet(4, -speed);
+}
+
+void clawSet(int speed) {
+	motorSet(7, -speed);
+}
+
+void operatorControl() {
+  int power;
+  int turn;
+
+    while (1) {
+
+
+      if(joystickGetDigital(1, 8, JOY_UP)){
+        homeShoulder(180);
+      }
+
+      if(joystickGetDigital(1, 8, JOY_DOWN)){
+        homeElbow(-70);
+      }
+
+      if(joystickGetDigital(1, 7, JOY_UP)){
+        clawSet(63);
+      }else if(joystickGetDigital(1, 7, JOY_DOWN)){
+        clawSet(-63);
+      }else{
+        clawSet(0);
+      }
+
+      if(joystickGetDigital(1, 7, JOY_LEFT)){
+        wristSet(63);
+      }else if(joystickGetDigital(1, 7, JOY_RIGHT)){
+        wristSet(-63);
+      }else{
+        wristSet(0);
+      }
+
+        power = joystickGetAnalog(1, 2); // vertical axis on left joystick
+        turn  = joystickGetAnalog(1, 1); // horizontal axis on left joystick
+				chassisSet((power+turn),(power-turn));
+
+        printf("The shoulder encoder value %d \n", encoderGet(encoder1));
+        if(joystickGetAnalog(1, 3) != 0){
+          shoulderSet(-1*joystickGetAnalog(1, 3));
+          target=encoderGet(encoder1);
+        }else{
+          int diff = encoderGet(encoder1)-target;
+          shoulderSet(diff*con);
+        }
+
+        printf("The elbow encoder value %d \n", encoderGet(encoder2));
+				if(joystickGetDigital(1, 6, JOY_UP)) {
+      motorSet(8,127); // pressing up, so lift should go up
+      target2=encoderGet(encoder2);
+    }else if(joystickGetDigital(1, 6, JOY_DOWN)) {
+      motorSet(8,-127); // pressing down, so lift should go down
+      target2=encoderGet(encoder2);
+    }
+    else {
+      motorSet(8,encoderGet(encoder2)-target2); // no buttons are pressed, stop the lift
+    }
+
+        delay(20);
+    }
+
+}
